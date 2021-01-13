e8524edf883827de9cbfbb17de6c3b169b2103b1
Channa96 <channa.kumara255@gmail.com>
2021-01-13 22:24:41 +0530

    webinit

---
+++ Web/item/index.php
@@ -0,0 +1,34 @@
+<?php
+
+ include "../CommenLayouts/header.php";
+
+ include "../CommenLayouts/navBar.php";
+
+ include "../CommenLayouts/sideDrawer.php";
+
+ //include "../CommenLayouts/banner.php";
+
+ include "../CommenLayouts/sideDrawerEnd.php";
+
+ //include "../CommenLayouts/adPanel.php";
+
+?>
+
+
+		<div class="w3l_banner_nav_right">
+			
+		<?php
+
+		include "../php/viewItem.php"
+		?>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+<!-- //banner -->
+
+
+
+<?php
+
+include "../CommenLayouts/footer.php";
+?>
