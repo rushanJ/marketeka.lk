c91b36f5214e8b7eda6ab8f38548bafead40d73b
Channa96 <channa.kumara255@gmail.com>
2021-01-13 22:23:37 +0530

    webinit

---
+++ Web/index/index.php
@@ -0,0 +1,32 @@
+<?php
+
+ include "../CommenLayouts/header.php";
+
+ include "../CommenLayouts/navBar.php";
+
+ include "../CommenLayouts/sideDrawer.php";
+
+ include "../CommenLayouts/banner.php";
+
+ include "../CommenLayouts/sideDrawerEnd.php";
+
+ //include "../CommenLayouts/adPanel.php";
+
+?>
+
+
+
+<?php
+
+include "searchItems.php";
+include "hotOffers.php";
+ 
+?>
+
+
+
+
+<?php
+include "js.php";
+include "../CommenLayouts/footer.php";
+?>
