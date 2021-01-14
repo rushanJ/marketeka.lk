090a717863cef799a3992288241f0c5d62fe4a04
sew999 <61828624+sew999@users.noreply.github.com>
2021-01-15 00:42:57 +0530

    OrderHistory

--- Web/seller/newOrders/title.php
+++
@@ -1,3 +0,0 @@
-<header class="w3-container" style="padding-top:22px">
-    <h5><b><i class="fa fa-dashboard"></i> NEW ORDERS</b></h5>
-  </header>
--- Web/seller/newOrders/viewOrders.php
+++ Web/seller/newOrders/viewOrders.php
@@ -1,19 +1,20 @@
-
+<div class=" w3-card-4 w3-padding-16 w3-panel w3-border w3-round-xlarge" style="margin:5%">
 <div class="w3-container">
+<h1><b><i class="fa fa-pencil-square-o"></i> New Orders</b></h1><br/>
 <!-- <a href="../addNewItem"><p><button class="w3-button w3-blue w3-round-xxlarge">Add New Item</button></p></a> -->
 <div style="overflow-x:auto;">
   <table  class="w3-table-all">
     <thead>
-      <tr class="w3-black">
-        <th>OrderId </th>
+      <tr class="w3-blue">
+        <th>Order ID </th>
         <th>Total Amount</th>
         <th>Address</th>
         <th># of Items</th>
         <th>Status</th>
         <th>Remarks</th>
         <th>Actions</th>
-        
       </tr>
+  
     </thead>
     <tbody>
 
@@ -24,5 +25,6 @@ include "../php/viewOrderList.php";
 
     </tbody>
   </table>
+</div>
 </div>
 </div>
