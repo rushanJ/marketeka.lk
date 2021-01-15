227419f649fbdb9c7381cd17616ed9c294b53811
kryptocj <vishwajith.9t@gmail.com>
2021-01-15 21:26:30 +0530

    banner change/navbar/images

--- Web/CommenLayouts/banner.php
+++ Web/CommenLayouts/banner.php
@@ -4,7 +4,7 @@
 				<div class="flexslider">
 					<ul class="slides">
 						<li>
-							<div class="w3l_banner_nav_right_banner">
+							<div class="w3l_banner_nav_right_banner1">
 								<!--<h3>Register your <span>Shop</span> and help.</h3>-->
 								<div class="more">
 									<!--<a href="../seller" class="button--saqui button--round-l button--text-thick" data-text="Register">Seller Area</a>-->S
@@ -53,7 +53,6 @@
 				</div>
 			</section>
 			<!-- flexSlider -->
-				<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
 				<script defer src="js/jquery.flexslider.js"></script>
 				<script type="text/javascript">
 				$(window).load(function(){
